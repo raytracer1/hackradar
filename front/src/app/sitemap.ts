9196226c2abfb3552709aa8ada1f5f38123eb31e
bijun <zhengbijun123@gmail.com>
2026-07-02 14:42:26 -0700

    add blog

--- front/src/app/sitemap.ts
+++ front/src/app/sitemap.ts
@@ -10,5 +10,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'hourly',
       priority: 1,
     },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
   ];
 }
